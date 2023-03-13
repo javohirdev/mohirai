45a65c2620afbc1ebd1f4df03f345732ebb40e52
Javohir Hakimov <57003102+javohirdev@users.noreply.github.com>
2023-03-13 14:46:38 +0500

    changes

--- component/ModalPartners.js
+++ component/ModalPartners.js
@@ -25,7 +25,7 @@ export default function ModalPartners({ setOpenPartnersModal }) {
   }
 
   const token = "5898057424:AAHPSH1xom0rFNHCHYN_9MgVJzdizDctelA";
-  const chad_id = -1001942371012;
+  const chad_id = "-1001942371012";
 
   const handleHCaptchaVerify = (responseToken) => {
     setHCaptchaResponse(responseToken);
